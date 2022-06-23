49d12912ab79f3611a1185a1d8d446a366be1221
C-katayama <katayma711@gmail.com>
2022-06-23 16:47:07 +0900

    html構造　#1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- svelte-ignore a11y-missing-attribute -->
+<html>
+    <head>
+
+    </head>
+    
+    <body>
+        <script>
+            // let backGround = "wallpaperbetter.com_3840x2160.jpg"
+        
+        </script>
+        
+        <div class="backGround">
+            <!-- <button type="button">adgrg</button> -->
+        </div>
+        
+        
+        
+        
+            <style>
+                .backGround {
+        
+                    background-image: url("https://www.wallpaperbetter.com/ja/hd-wallpaper-aswom/download/3072x1920");
+                    background-image: url("https://pro-foto.jp/free/img/images_big/wtr0074-024.jpg");
+                    /* background-image: url("https://www.wallpaperbetter.com/ja/hd-wallpaper-cqipg/download/3072x1920.jpg"); */
+                }
+            </style>
+        
+    </body>
+    </html>
